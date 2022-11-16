f2a516f9a2c25eed9b25002fa476afdb80e8fd88
Anna Beatriz <annab_oliver@hotmail.com>
2022-11-16 14:02:38 -0300

    inicia Js

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome = prompt("Informe seu nome:");
+let sobrenome = prompt("Informe seu sobrenome:");
+
+document.write ("Nome completo: " + nome + " " + sobrenome);
+document.write("<br>");
+document.write ("Nome de catálago: " + sobrenome.toUpperCase() + ", " + nome);
